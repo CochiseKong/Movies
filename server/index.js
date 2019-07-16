d1354857bb27cade423abc406cd174ef1c8adc3e
Keegan <keegandau@qq.com>
2019-07-16 21:48:00 +0800

    增加模板引擎

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const Koa = require("koa")
 const app = new Koa
-const { normal } = require('./tpl/index.js')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-  ctx.type = "text/html;charset=utf-8"
-  ctx.body = normal
+  await ctx.render('index',{
+    you:'Luck',
+    me: 'Scott'
+  })
 })
+
 app.listen(8080)
