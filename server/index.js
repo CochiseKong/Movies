2f00f082ba4a0afed0be6aeb3e4fe62dba75b84c
Keegan <keegandau@qq.com>
2019-07-17 11:26:53 +0800

    增加web目录

--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 const Koa = require("koa")
 const app = new Koa
 const views = require('koa-views')
-const { resolve } = require('path')
+const statics = require('koa-static')
+const path = require('path')
 
-app.use(views(resolve(__dirname, './views'), {
-  extension: 'pug'
-}))
+const staticPath = './static'
 
-app.use(async (ctx, next) => {
-  await ctx.render('index',{
-    you:'Luck',
-    me: 'Scott'
-  })
-})
+app.use(statics(
+  path.join(__dirname, staticPath)
+))
 
 app.listen(8080)
